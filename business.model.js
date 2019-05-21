47650a6177b672a5c2a1d92e65e3ad9e93a27dca
dracero <dracero@users.noreply.github.com>
2019-05-21 20:38:35 -0300

    Update business.model.js

--- business.model.js
+++ business.model.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define collection and schema for Business
-let webPush = new Schema({
-  objectID: {
-    type: Map,
-    of: String
-   }
-},{
-    collection: 'oldstore'
-});
+let webPush = new Schema({myJsonProperty: JSON.stringify(objectID)},
+                         {collection: 'oldstore'});
 
 module.exports = mongoose.model('webPush', webPush);
