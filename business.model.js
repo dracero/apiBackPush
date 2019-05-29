61fe5c98803f0c959b72467c4f711195ac6fa4d7
dracero <dracero@users.noreply.github.com>
2019-05-29 15:20:47 -0300

    Update business.model.js

--- business.model.js
+++ business.model.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 // Define collection and schema for Business
 let webPush = new Schema(
-  {
+  { variable: Object
+    /*
     endpoint: String,
     expirationdate: String,
-    keys:[{ p256h: String, auth: String }]
+    keys:[{ p256h: String, auth: String }]*/
   },
   { collection: "oldstore" }
 );
