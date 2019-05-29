09e11290ff2e6a5c06ca93e769384396619a5688
dracero <dracero@users.noreply.github.com>
2019-05-29 09:37:55 -0300

    Update business.model.js

--- business.model.js
+++ business.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Define collection and schema for Business
 let webPush = new Schema({
- myJsonProperty: String},
+ myJsonProperty: Object},
  {collection: 'oldstore'});
 
 
