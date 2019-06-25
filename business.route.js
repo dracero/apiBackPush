8fd385b45b2ae8a89b313dd7e087803d71e2ca11
dracero <dracero@users.noreply.github.com>
2019-06-25 19:18:55 -0300

    Update business.route.js

--- business.route.js
+++ business.route.js
@@ -59,7 +59,7 @@ businessRoutes.route('/update/:id').post(function (req, res) {
 
 // Defined delete | remove | destroy route
 businessRoutes.route('/delete/:variable').get(function (req, res) {
-   Business.findOneAndRemove({'subscription.endpoint':req.params.variable}, function(err, business){
+   Business.findOneAndRemove({variable:req.params.variable}, function(err, business){
         if(err) res.json(err);
         else res.status(200).json({'Delete': 'business in deleted successfully'});
     });
